764b34a66c6ec0a67f45ce31d19f39da36773b47
furi diningrat <63885192+furidngrt@users.noreply.github.com>
2020-10-05 15:04:32 +0700

    Update

--- hacktober3.py
+++ hacktober3.py
@@ -28,3 +28,9 @@ def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
 # Driver code 
 n = 4
 TowerOfHanoi(n, \'A\', \'C\', \'B\') 
+
+             
+             C:\Users\Your Name>python
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello, World!")
